74f624b80be1717d0e6742948672acf00091f330
Park So Dam <parksodam0228@e-mirim.hs.kr>
2018-03-26 15:01:42 +0900

    2018.03.26 퍼즐 프로젝트 완성

--- Aurum/Lecture_3606/CSharp/C01.HelloWorld/Chapter08_01/Form1.Designer.cs
+++ Aurum/Lecture_3606/CSharp/C01.HelloWorld/Chapter08_01/Form1.Designer.cs
@@ -28,21 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 481);
+            this.ClientSize = new System.Drawing.Size(504, 438);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Aurum/Lecture_3606/CSharp/C01.HelloWorld/Chapter08_01/Form1.cs
+++ Aurum/Lecture_3606/CSharp/C01.HelloWorld/Chapter08_01/Form1.cs
@@ -29,7 +29,33 @@ namespace Chapter08_01
                 Image tmpI = Image.FromFile(tmpName);
                 imgList.Add(tmpI);
             }
+            theTick = 0;
+            theGameTick = 0;
+
+            timer1.Start();
+
+            Random tmpR = new Random();
+            theViewIndices = new int[16];
+            for(i=0; i < 16; i++)
+            {
+                theViewIndices[i] = i;
+            }
+            for (i = 0; i < 1000; i++)
+            {
+                int tmpSwap;
+                int tmpRand1 = tmpR.Next(16);
+                int tmpRand2 = tmpR.Next(16);
+                tmpSwap = theViewIndices[tmpRand1];
+                theViewIndices[tmpRand1]=theViewIndices[tmpRand2];
+                theViewIndices[tmpRand2] = tmpSwap;
+            }
+            theEmptyIndex = 15;
+            theTouchIndex = -1;
+
+            bSuccess = 0;
         }
+
+
         Pen thePen;
         Brush theBrush;
         Font theFont;
@@ -38,20 +64,122 @@ namespace Chapter08_01
 
         int scrX = 50;
         int scrY = 50;
+        int theTick;
+        int theGameTick;
+
+        int[] theViewIndices;
+        int theEmptyIndex;
+        int theTouchIndex;
+        int bSuccess;
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            String strTime = String.Format("Time:{0:D3}", 99);
+            String strTime = String.Format("Time:{0:D3}", 99-(theTick-theGameTick)/20);
 
-            e.Graphics.DrawRectangle(thePen, 0, 0, 100, 10);
+            e.Graphics.DrawRectangle(thePen, 0, 0, 99-(theTick-theGameTick)/20, 10);
             e.Graphics.DrawString(strTime, theFont, theBrush, 0, 10);
 
             int i;
             for (i = 0; i < 16; i++)
             {
-                e.Graphics.DrawImage(imgList[i],
+                if(theViewIndices[i]==theEmptyIndex)
+                {
+                    continue;
+                }
+                e.Graphics.DrawImage(imgList[theViewIndices[i]],
                     scrX + (i % 4) * 100, scrY + (i / 4) * 100, 100, 100);
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            theTick++;
+            Invalidate();
+        }
+        int[][] theWays =
+        {
+            new int[] {1,4},
+            new int[] {0,2,5},
+            new int[] {1,3,6},
+            new int[] {2,7},
+            new int[] {0,5,8},
+            new int[] {1,4,6,9},
+            new int[] {2,5,7,10},
+            new int[] {3,6,11},
+            new int[] {4,9,12},
+            new int[] {5,8,10,13},
+            new int[] {6,9,11,14},
+            new int[] {7,10,15},
+            new int[] {8,13},
+            new int[] {9,12,14},
+            new int[] {10,13,15},
+            new int[] {11,14}
+        };
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int i;
+            int tmpX = e.X;
+            int tmpY = e.Y;
+            int tmpDp = 100;
+            theTouchIndex = -1;
+            for (i = 0; i < 16; i++)
+            {
+                if(scrX+(i%4)*tmpDp<tmpX&&tmpX<scrX+(i%4)*tmpDp+tmpDp)
+                {
+                    if(scrY+(i/4)*tmpDp<tmpY&&tmpY<scrY+(i/4)*tmpDp+tmpDp)
+                    {
+                        theTouchIndex = i;
+                        break;
+                    }
+                }
+            }
+            if (0 <= theTouchIndex && theTouchIndex < 16)
+            {
+                for (i = 0; i < theWays[theTouchIndex].Length; i++)
+                {
+                    int tmpWayPos = theWays[theTouchIndex][i];
+                    if (theViewIndices[tmpWayPos] == theEmptyIndex)
+                    {
+                        int tmpSwap;
+                        tmpSwap = theViewIndices[tmpWayPos];
+                        theViewIndices[tmpWayPos] = theViewIndices[theTouchIndex];
+                        theViewIndices[theTouchIndex] = tmpSwap;
+                    }
+                }
+                bSuccess = CheckSuccess();
+            }
+            Invalidate();
+            if (bSuccess == 1)
+            {
+                MessageBox.Show("완료했습니다.");
+            }
+        }
+        public int CheckSuccess()
+        {
+            int i;
+            for (i = 0; i < 16; i++)
+            {
+                if (theViewIndices[i] == theEmptyIndex)
+                {
+                    continue;
+                }
+                if (theViewIndices[i] == i)
+                {
+                    continue;
+                }
+                break;
+            }
+            if (i == 16)
+            {
+                return (1);
+            }
+            return (0);
         }
     }
 }
